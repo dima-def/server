b23eda8404bd53ee01bf2c711f78048aa155d8ed
Maxim Kovtun <kovtun.m@triadasite.ru>
2025-04-12 14:34:35 +0300

    hello world

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return jsonify({'message': 'Car added successfully!'}), 201
+    return jsonify({'message': 'Hello world!'}), 201
 
 if __name__ == "__main__":
   app.run(debug=True)
