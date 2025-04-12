ee41fb9540ff8b70f0b22a682726117976bfc5be
Maxim Kovtun <kovtun.m@triadasite.ru>
2025-04-12 14:55:42 +0300

    mysql commened

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-from flask_mysqldb import MySQL
+#from flask_mysqldb import MySQL
 from dotenv import load_dotenv
 import os
 
@@ -14,7 +14,7 @@ app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 
-mysql = MySQL(app)
+#mysql = MySQL(app)
 
 @app.route('/')
 def hello_world():
