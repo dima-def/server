b9ae858661641d680d64e718c60778c736a3d380
Maxim Kovtun <kovtun.m@triadasite.ru>
2025-04-15 21:59:34 +0300

    Create method was added

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL
 import os
 
@@ -10,11 +10,31 @@ app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 
-#mysql = MySQL(app)
+mysql = MySQL(app)
 
 @app.route('/')
 def hello_world():
     return jsonify({'message': 'Hello world!'}), 201
+
+# Создание машины (Create)
+@app.route('/cars', methods=['POST'])
+def add_car():
+    data = request.get_json()
+    brand = data['brand']
+    model = data['model']
+    color = data['color']
+    license_plate = data['license_plate']
+    
+    cur = mysql.connection.cursor()
+    cur.execute(
+        "INSERT INTO cars (brand, model, color, license_plate) VALUES (%s, %s, %s, %s)",
+        (brand, model, color, license_plate)
+    )
+    mysql.connection.commit()
+    cur.close()
+    
+    return jsonify({'message': 'Car added successfully!'}), 201
+
 
 if __name__ == "__main__":
   app.run(debug=True)
